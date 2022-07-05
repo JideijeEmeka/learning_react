7a430e172245af1794794feffce4f91678238383
Emeka Jideije <jideije.emeka@gmail.com>
2022-07-05 17:33:57 +0100

    Learning react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import { Greet } from './components/Greet';
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-       
+       <Greet/>
       </div>
     );
   }
