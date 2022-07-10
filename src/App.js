7616fffd5b3458736cea75dceae2f03b00117b62
Emeka Jideije <jideije.emeka@gmail.com>
2022-07-10 18:48:17 +0100

    Learning event binding

--- src/App.js
+++ src/App.js
@@ -3,25 +3,33 @@ import './App.css';
 import { Component } from 'react';
 // import Greet from './components/Greet';
 // import Welcome from './components/welcome';
+// import FunctionClick from './components/functionClick';
+// import ClassClick from './components/classClick';
+//import EventBind from './components/eventBind';
+import ParentComponent from './components/parentComponent';
 // import Hello from './components/hello';
-import Message from './components/message';
-import Counter from './components/counter';
+// import Message from './components/message';
+// import Counter from './components/counter';
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <Counter/>
+        {/* <EventBind/> */}
+        <ParentComponent/>
+      {/* <FunctionClick/>
+      <ClassClick/> */}
+        {/* <Counter/> */}
         {/* <Message></Message> */}
        {/* <Greet name = 'Bruce' heroName = 'Batman'>
          <p>This is children props</p>
        </Greet>
        <Greet name = 'Clark' heroName = 'Superman'>
          <button>Action</button>
-       </Greet>
-       <Greet name = 'Diana' heroName = 'WonderWoman'/>
-       <Welcome name = 'Bruce' heroName = 'Batman'/>
-       <Welcome name = 'Clark' heroName = 'Superman'/>
+       </Greet> */}
+       {/* <Greet name = 'Diana' heroName = 'WonderWoman'/>
+       <Welcome name = 'Bruce' heroName = 'Batman'/> */}
+       {/* <Welcome name = 'Clark' heroName = 'Superman'/>
        <Welcome name = 'Diana' heroName = 'WonderWoman'/> */}
        {/* <Hello/> */}
       </div>
